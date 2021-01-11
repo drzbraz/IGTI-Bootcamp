17e9ab674c80f87ce5d5c3eaaeeec87fc5942008
Daniel Braz <daniel.braz@mundiale.com.br>
2021-01-11 08:44:01 -0300

    feat: add challenge nodejs

--- TP-1-Node/app.js
+++ TP-1-Node/app.js
@@ -1,25 +1,69 @@
-import {promises as fs} from 'fs'
-async function searchState() {
+import { promises as fs } from 'fs'
+import readline from 'readline'
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+async function createFile() {
+    try {
+        const cities = JSON.parse(await fs.readFile('Cidades.json', 'utf-8'))
+        const states = JSON.parse(await fs.readFile('Estados.json', 'utf-8'))
 
-  try {
-    const citites = []    
-    const city = JSON.parse(await fs.readFile("Cidades.json", "utf-8"))
-    const state = JSON.parse(await fs.readFile('Estados.json', "utf-8"))
-
-    for (let i = 0; i < city.length; i++){
-      for (let j = 0; j< state.length; j++){
-        if (city[i].Estado === state[j].ID) {         
-          await fs.appendFile(`${(state[j].Sigla)}.json`,JSON.stringify(city[i]))
-        }
-
-      }
+        states.map((states) => {
+            const findCity = cities.filter((city) => {
+                if (states.ID === city.Estado) return city
+            })
+            fs.writeFile(
+                `./states/${states.Sigla}.json`,
+                JSON.stringify({ cidades: findCity })
+            )
+        })
+    } catch (error) {
+        console.log(error)
     }
-    console.log(citites)
-    // if (city).Estado === JSON.parse(state).ID) {
-     // }
-  } catch (error) {
-    console.log(error)    
-  }
 }
 
-searchState()
+async function citiesInState(UF) {
+    const state = JSON.parse(await fs.readFile(`./states/${UF}`, 'utf-8'))
+
+    return state.cidades.length
+}
+async function statesWithBigCities() {
+    const citiesNumber = []
+    const files = await fs.readdir('./states')
+    let value = 0
+    for (let index = 0; index < files.length; index++) {
+        citiesNumber.push(
+            (await citiesInState(files[index])) +
+                '-' +
+                files[index].toString().split('.')[0]
+        )
+    }
+    console.log(
+        citiesNumber
+            .sort(function (a, b) {
+                return parseInt(b.split('-')[0]) - parseInt(a.split('-')[0])
+            })
+            .splice(0, 5)
+    )
+
+    // for (let index = 0; index < citiesNumber.length; index++) {
+    //     if ()
+
+    //         const element = array[index];
+
+    // }
+    // console.log(citiesNumber)
+}
+
+async function question() {
+    rl.question('Digite UF de um estado: ', async (UF) => {
+        const number = await citiesInState(UF)
+        console.log(`O número de cidades desse estado é de ${number}`)
+        rl.close()
+    })
+}
+
+createFile()
+statesWithBigCities()
+// question()
